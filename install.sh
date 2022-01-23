f4e9b30469a9be078e58ac5c32405ac396ad9950
impactaky <impactaky@users.noreply.github.com>
2022-01-23 23:21:00 +0900

    Don't generate my_conf.vimrc

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ ln -s $current_dir/.vimrc $HOME/.vimrc
 ln -s $current_dir/.gvimrc $HOME/.gvimrc
 ln -s $current_dir/ $HOME/.vim
 ln -s $current_dir/ $HOME/.config/nvim
-cp ./template.vimrc ./my_conf.vimrc
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh $HOME/.dein
