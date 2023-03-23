c20fb1e2fd735a4f27bf881a92d73c610c3f1f74
impactaky <impactaky@users.noreply.github.com>
2023-03-23 21:45:44 +0900

    Denite for nvim-lsp

--- plugin_conf/nvim-lspconfig.lua
+++ plugin_conf/nvim-lspconfig.lua
@@ -4,3 +4,5 @@ nvim_lsp.clangd.setup {
         vim.g['my_clangd']
     },
 }
+vim.api.nvim_set_keymap("n", "g]", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "g[", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
