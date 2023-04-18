bdbd621bd2259fd1d4b0548c9326fe3abbb33002
impactaly <impactaky@users.noreply.github.com>
2023-04-18 19:42:08 +0900

    Add config for zig

--- plugin_conf/nvim-lspconfig.lua
+++ plugin_conf/nvim-lspconfig.lua
@@ -1,8 +1,10 @@
 local nvim_lsp = require('lspconfig');
+nvim_lsp.bashls.setup {}
 nvim_lsp.clangd.setup {
     cmd = {
         vim.g['my_clangd']
     },
 }
+nvim_lsp.zls.setup {}
 vim.api.nvim_set_keymap("n", "g]", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("n", "g[", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
